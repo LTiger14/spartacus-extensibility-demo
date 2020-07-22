852f18625275a49227014f517a9650a93beee7f2
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2020-07-22 16:04:46 -0400

    Spartacus changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { translationChunksConfig, translations } from '@spartacus/assets';
+import { B2cStorefrontModule } from '@spartacus/storefront';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://spartacus-dev0.eastus.cloudapp.azure.com:9002',
+          prefix: '/occ/v2/',
+        },
+      },
+      context: {
+        baseSite: ['electronics-spa'],
+        currency: ['USD'],
+        language: ['en'],
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en',
+      },
+      features: {
+        level: '2.0',
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
